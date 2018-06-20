bfefe0fa449506f9bdb7aaa8ca082c989778ca1b
spandey <spandey@deerwalk.com>
2018-06-20 14:07:43 +0545

    teaching variable declaration

---
+++ src/variables/VariableExample.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class VariableExample {
+
+    public static void main(String args[]){
+        int a;
+        int b,c,d,e;
+        int f = 0;
+
+
+        a = 5;
+
+        //uncommenting this line will give error as save variable within same body cannot be declared multiple times
+        //int b = 10;
+
+    }
+}
