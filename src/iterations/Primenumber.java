528c5eca2370a456c22f48bb4bf3172b1bb1c763
dtsushant <dtsushant@gmail.com>
2018-08-23 08:21:05 +0545

    while example

--- src/iterations/Primenumber.java
+++ src/iterations/Primenumber.java
@@ -1,4 +1,16 @@
 package iterations;
 
 public class Primenumber {
+    public static void main(String args[]){
+        for(int i = 2; i<=1000;i++){
+            int count = 0;
+            for(int j = 2; j<=i/2;j++){
+
+
+            }
+            if(count>0){
+
+            }
+        }
+    }
 }
--- src/iterations/WhileLoopExample.java
+++ src/iterations/WhileLoopExample.java
@@ -1,4 +1,47 @@
 package iterations;
 
 public class WhileLoopExample {
+    public static void main(String args[]){
+        int i = 1;
+        while(i<=10){
+            System.out.print(i+" ");
+
+            i++;
+        }
+
+        System.out.println("\nin reverse order");
+        while(true){
+            System.out.print( i + " ");
+            i--;
+            if(i==0)
+                break;
+        }
+
+        int sumOfEvenInteger = 0;
+        int sumOfOddInteger = 0;
+        String stringOfEvenInteger="";
+        String stringOfOddInteger="";
+
+        while(i<=100){
+            if (i % 2 == 0) {
+                sumOfEvenInteger = sumOfEvenInteger + i;
+                stringOfEvenInteger = stringOfEvenInteger+"+"+i;
+            }else{
+                sumOfOddInteger = sumOfOddInteger + i;
+                stringOfOddInteger = stringOfOddInteger+"+"+i;
+            }
+            i++;
+        }
+
+        System.out.printf("\n%s = %d",stringOfEvenInteger,sumOfEvenInteger);
+        System.out.printf("\n%s = %d",stringOfOddInteger,sumOfOddInteger);
+
+        do{
+            System.out.println("\nthe final value of i = "+i);
+            i++;
+        }while(i<=100);
+
+
+
+    }
 }
