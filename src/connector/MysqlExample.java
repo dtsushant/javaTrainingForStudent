7e992d0fe1b244bd95100bcfc56a6303b26b61a9
spandey <spandey@deerwalk.com>
2018-09-17 07:52:04 +0545

    mysqlconnection select example

--- src/connector/MysqlExample.java
+++ src/connector/MysqlExample.java
@@ -1,6 +1,7 @@
 package connector;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class MysqlExample {
@@ -9,7 +10,7 @@ public class MysqlExample {
         try{
             con = MysqlConnector.getConnection();
 
-            String query = "insert into student(full_name,grade, age, gender) values('something something','3','7','male');";
+            String query = "insert into student(full_name,grade, age, gender) values('Ram Bahadur','3','7','male');";
             Statement stmt = con.createStatement();
             stmt.execute(query);
 
@@ -21,6 +22,41 @@ public class MysqlExample {
     }
 
     public static void main(String args[]){
-        insertExample();
+        //insertExample();
+       selectExample();
+    }
+
+    public static void selectExample(){
+        Connection con = null;
+        try{
+            con = MysqlConnector.getConnection();
+
+            String query = "select * from student;";
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()){
+                System.out.println("\nprinting information of one of the row:- \n------------------------");
+                System.out.print(rs.getInt("id") +" ");
+                System.out.print(rs.getString("full_name") +" ");
+                System.out.print(rs.getString("grade") +" ");
+                System.out.print(rs.getString("age") +" ");
+                System.out.print(rs.getString("gender") +" ");
+            }
+
+            con.close();
+
+        }catch (Exception ex){
+            System.out.println(ex.toString());
+        }
+    }
+
+    public static void updateExample(){
+        //update student set full_name = "someone something" where id = 3;
+    }
+
+    public static void deleteExample(){
+       // delete from student where id = 4;
+
     }
 }
