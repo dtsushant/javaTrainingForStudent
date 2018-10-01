5744f9096e8f34b19121d2870de7fe4c1ece7b07
spandey <spandey@deerwalk.com>
2018-10-01 15:43:53 +0545

    variable example

---
+++ src/variables/VariableExample.java
@@ -0,0 +1,45 @@
+package variables;
+
+public class VariableExample {
+
+    public static void main(String args[]){
+        int a = 3;
+        int b, c,d ;
+        b = 5;
+        c = 10;
+        d = b + c;
+
+        System.out.println(a);
+
+        System.out.println(d);
+
+
+        float f1, f2, f3;
+        f1 = 4.5f;
+        f2 = 5.6f;
+        f3 = f1 + f2;
+
+        System.out.println(f3);
+
+        double d1, d2, d3;
+        d1 = 5.5;
+        d2 = 6.7;
+        d3 = d1 + d2;
+
+        System.out.println(d3);
+
+        char c1, c2, c3;
+        int i;
+        c1 = 'a';
+        c2 = 'A';
+
+        i = c1 + c2;
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(0+c1);
+        System.out.println(0+c2);
+
+        System.out.println(c1 + c2);
+
+    }
+}
