0000d76fbf954540cfc296b8a4af2503f31cad29
spandey <spandey@deerwalk.com>
2018-10-04 15:48:05 +0545

    classwork

---
+++ src/conditionals/ArithmeticOperation.java
@@ -0,0 +1,16 @@
+package conditionals;
+
+public class ArithmeticOperation {
+    public static void main(String args[]){
+        int a,b;
+        int operation;
+        a = 100;
+        b = 200;
+        char operate ='+';
+
+
+        switch (operate){
+            //complete the code
+        }
+    }
+}
