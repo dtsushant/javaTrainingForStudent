f2886423f837ad4b48e76377394fd784ff23e781
spandey <spandey@deerwalk.com>
2018-10-08 15:35:47 +0545

    looping statement example

--- src/iteration/WhileLoop.java
+++ src/iteration/WhileLoop.java
@@ -20,15 +20,17 @@ public class WhileLoop {
         // 0 1 1 2 3 5 8 13 21
 
         //printing fibonacci number
-
         int j = 0;
         int fibo0 = 0;
         int fibo1 = 1;
+        int fibo=0;
 
         while(j < 10){
+
             System.out.print(fibo0 + " ");
+            fibo = fibo0+fibo1;
             fibo0 = fibo1;
-            fibo1 = fibo0+fibo1;
+            fibo1 = fibo;
             j++;
         }
     }
