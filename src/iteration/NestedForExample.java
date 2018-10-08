8b6954dfaf3f138800f7e7e225818de4da5276e5
spandey <spandey@deerwalk.com>
2018-10-08 15:47:50 +0545

    looping statement example

---
+++ src/iteration/NestedForExample.java
@@ -0,0 +1,25 @@
+package iteration;
+
+public class NestedForExample {
+    public static void main(String args[]){
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println("************** Multiplication table of "+i+"**************");
+            for(int j = 1; j <= 10; j++){
+                System.out.printf("%d * %d = %d \n",i, j, i*j );
+            }
+
+        }
+
+        //printing all prime no. within 1000
+
+        for(int i = 2; i<=1000;i++){
+            boolean check = false;
+            for(int j = 2; j <= i/2; j++){
+                if(i%j==0){
+
+                }
+            }
+        }
+    }
+}
