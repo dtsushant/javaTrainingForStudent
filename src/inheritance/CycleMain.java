44f33f6ae7629560f15065bcc9585948e82fb83e
spandey <spandey@deerwalk.com>
2018-11-01 15:33:23 +0545

    overridding example

--- src/inheritance/CycleMain.java
+++ src/inheritance/CycleMain.java
@@ -5,7 +5,7 @@ public class CycleMain {
     public static void main(String args[]){
         Cycle c1 = new Cycle();
         c1.printSpeed();
-        c1.pedalPerSec = 6.0;
+        c1.pedalPerSec = 8.0;
         c1.printSpeed();
 
         Cycle c2 = new Cycle(5);
@@ -15,5 +15,11 @@ public class CycleMain {
         gc1.printSpeed();
         gc1.pedalPerSec = 8.0;
         gc1.printSpeed();
+
+        GearWalaCycle gc2 = new GearWalaCycle(5);
+        gc2.printSpeed();
+
+        GearWalaCycle gc3 = new GearWalaCycle(5,2);
+        gc3.printSpeed();
     }
 }
--- src/inheritance/GearWalaCycle.java
+++ src/inheritance/GearWalaCycle.java
@@ -1,4 +1,48 @@
 package inheritance;
 
 public class GearWalaCycle extends Cycle {
+
+    Integer gear;
+
+    public GearWalaCycle(){
+        super();
+        this.gear = 3;
+    }
+
+    public GearWalaCycle(double pedalPerSec){
+        super(pedalPerSec);
+        this.gear=3;
+    }
+
+    public GearWalaCycle(double pedalPerSec, Integer gear){
+        super(pedalPerSec);
+        if(gear<1 || gear>5)
+            gear =3;
+        this.gear = gear;
+    }
+
+    public Double getSpeed(){
+        double speed = 0.0;
+        switch (gear){
+            case 1:
+                speed = pedalPerSec * 10;
+                break;
+            case 2:
+                speed = pedalPerSec * 8;
+                break;
+            case 3:
+                speed = super.getSpeed();
+                break;
+            case 4:
+                speed = pedalPerSec * 4;
+                break;
+            case 5:
+                speed = pedalPerSec * 5;
+        }
+        return speed;
+    }
+
+    public String maker(){
+        return " Giant";
+    }
 }
