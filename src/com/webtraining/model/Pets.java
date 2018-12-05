80ef70d182488939a94ce744c45e7e9913d48185
spandey <spandey@deerwalk.com>
2018-12-05 15:50:26 +0545

    jsp

---
+++ src/com/webtraining/model/Pets.java
@@ -0,0 +1,90 @@
+package com.webtraining.model;
+
+public class Pets {
+    private Integer id;
+    private String name;
+    private String type;
+    private Integer     age;
+    private String  color;
+    private boolean isVaccinated;
+    private Double  weight;
+
+    public Pets() {
+    }
+
+
+    public Pets(String name, String type, Integer age, String color, boolean isVaccinated, Double weight) {
+        this.name = name;
+        this.type = type;
+        this.age = age;
+        this.color = color;
+        this.isVaccinated = isVaccinated;
+        this.weight = weight;
+    }
+
+    public Pets(Integer id, String name, String type, Integer age, String color, boolean isVaccinated, Double weight) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.age = age;
+        this.color = color;
+        this.isVaccinated = isVaccinated;
+        this.weight = weight;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isVaccinated() {
+        return isVaccinated;
+    }
+
+    public void setVaccinated(boolean vaccinated) {
+        isVaccinated = vaccinated;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+}
---
+++ web/something.jsp
@@ -0,0 +1,22 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: spandey
+  Date: 12/5/18
+  Time: 3:17 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>something</title>
+</head>
+<body>
+
+<%
+    for (int i =1; i<=10;i++){
+        out.println(i+"<br/>");
+    }
+%>
+
+</body>
+</html>
