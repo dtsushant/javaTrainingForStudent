d8d34cceec3d744a1ed3ceed7a7bfe0ad4998a68
spandey <spandey@deerwalk.com>
2019-01-23 16:10:21 +0545

    variables

--- src/variables/VariableExample.java
+++ src/variables/VariableExample.java
@@ -4,5 +4,23 @@ public class VariableExample {
 
     public static void main(String args[]){
 
+        // declaring a integer variable
+        int a;
+        // assigning a value
+        a = 10;
+
+        System.out.println(a);
+
+
+        // declaring an integer an and assigning a value
+        int b = 100;
+        System.out.println(b);
+
+        int c ;
+
+        // operation on a variable
+        c = a + b;
+
+        System.out.println(c);
     }
 }
