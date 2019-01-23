9dcfb4380ade9a4d796804710532c91197cd1c07
spandey <spandey@deerwalk.com>
2019-01-23 15:31:04 +0545

    startup program

---
+++ src/variables/HelloWorldExample.java
@@ -0,0 +1,8 @@
+package variables;
+
+public class HelloWorldExample {
+
+    public static void main(String args[]){
+        System.out.println("hello world this is my first java program");
+    }
+}
---
+++ src/variables/VariableExample.java
@@ -0,0 +1,8 @@
+package variables;
+
+public class VariableExample {
+
+    public static void main(String args[]){
+
+    }
+}
