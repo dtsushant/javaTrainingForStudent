80454b4b3b0811bbeaa967107480a246daad0e2a
spandey <spandey@deerwalk.com>
2019-01-23 16:34:22 +0545

    variables

--- src/variables/VariableExample.java
+++ src/variables/VariableExample.java
@@ -22,5 +22,31 @@ public class VariableExample {
         c = a + b;
 
         System.out.println(c);
+
+
+        //this cant be done and will give error
+        //a = 9.0;
+
+        float f1,f2,f3;
+
+        f1 = 78.5f;
+        System.out.println(f1);
+
+        f2 = 56/11;
+
+
+        System.out.println(f2);
+
+        f3 = 22.0f/7.0f;
+        System.out.println("f3 = "+f3);
+
+        double d1, d2,d3;
+        d1 = 9.6;
+
+        d2 = 22.0/7.0;
+        System.out.println("d2="+d2);
+
+
+
     }
 }
