650a7717756fb79c2bae85093d357d6432e6b091
spandey <spandey@deerwalk.com>
2019-02-01 16:09:37 +0545

    Method examples

--- src/objects/MethodExample.java
+++ src/objects/MethodExample.java
@@ -18,4 +18,28 @@ public class MethodExample {
         String beautifiedMultiple = String.format("the multiple of %f * %f is %f", one, two, one*two);
         return beautifiedMultiple;
     }
+
+    public String reverse(String s){
+        String r = "";
+        for(int i =s.length()-1; i>=0;i--)
+            r+=s.charAt(i);
+        return r;
+    }
+
+    public void printDogProperty(String name, int age, double weight){
+        System.out.printf("the name of the dog is %s, its age is %d and its weight is %f \n",name,age,weight);
+    }
+
+
+    public boolean isPalindrome(String s){
+        String r = reverse(s);
+
+        boolean bool = r.equalsIgnoreCase(s);
+
+        return bool;
+
+    }
+
+
+
 }
--- src/objects/MethodMainImplementation.java
+++ src/objects/MethodMainImplementation.java
@@ -29,5 +29,42 @@ public class MethodMainImplementation {
 
         System.out.println(beauMul2);
 
+        String s = me2.reverse("reverse");
+        System.out.println(s);
+
+        me2.printDogProperty("Kale",4,mul2);
+
+        boolean bool1 = me2.isPalindrome("liril");
+
+        if (bool1){
+            System.out.println(" the value passed was a palindrome");
+        }
+
+        if(me2.isPalindrome("test")){
+            System.out.println(" the value passed was a palindrome");
+        }else
+            System.out.println(" the value passed was not a palindrome");
+
+
+        /*
+
+            create a method that accepts 1 integer parameter and returns boolean value true if it is prime
+                eg. public boolean isPrime(int p)
+
+            create a void method that accepts any integer and would print its multiplication table from 1 to 10
+
+
+            create multiple interrelated methods that would work with 5 double value  as school subject and 1 String value to represent student name and prints if the students passed or failed with ranking
+                eg.
+                    this method shows the rank of student is student passed
+                        public String rank(double total)
+                    this method shows if the student has passed
+                        public boolean isPassed(double math, double english, ..., double nepali)
+                    this prints the student info in detail
+                        public void studentInfo(String name,double math, double english, ... double nepali)
+
+
+         */
+
     }
 }
